08544a60cf596cd752399dffe866f189d1c31228
rvronski <rvronski@gmail.com>
2023-01-06 03:20:26 +0500

    add RealmUser

---
+++ CustomTabBar/RealmLoginModel.swift
@@ -0,0 +1,23 @@
+//
+//  RealmLoginModel.swift
+//  Navigation
+//
+//  Created by ROMAN VRONSKY on 02.01.2023.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmUser: Object {
+    
+    @Persisted var login: String
+    @Persisted var password: String
+   convenience init(login: String, password: String) {
+        self.init()
+        self.login = login
+        self.password = password
+       
+    }
+}
+
+
